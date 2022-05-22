45e2d2c787dfda310556a6d97fec80770aabd112
Marcus Chiu <marcuschiu9@gmail.com>
2022-05-22 14:38:16 -0500

    ldap example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapContextSource;
 
+import javax.naming.directory.DirContext;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.IntSupplier;
@@ -19,18 +24,41 @@ public class DemoApplication implements CommandLineRunner {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello, World");
-        IntSupplier t = "hello"::length;
-        Consumer<Integer> function = System.out::println;
-        Consumer<Builder> c = null;
-        Map<String, Supplier<Double>> map = new HashMap<>();
-        c.accept(map::put);
-        log.info("my name is HAHAHAHAHHAHAHAHAHAHHAHAHAHA\n\t{} and {}", new Integer(123), "Hello, World");
+    @Autowired
+    public LdapTemplate ldapTemplate() {
+        LdapContextSource contextSource = new LdapContextSource();
+        contextSource.setUrl("ldap://ldap.example.og:389");
+        contextSource.setUserDn("cn=admin,dc=example,dc=org");
+        contextSource.setPassword("admin");
+
+        try {
+            contextSource.afterPropertiesSet();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        LdapTemplate template = new LdapTemplate();
+        template.setContextSource(contextSource);
+        return template;
     }
 
-    public interface Builder {
-        void register(String str, Supplier<Double> supplier);
+    @Autowired
+    private LdapTemplate ldapTemplate;
+
+    @Autowired
+    private LdapContextSource ldapContextSource;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("hello world");
+//        List<String> children = ldapTemplate.list("dc=example,dc=org");
+
+        DirContext dirContext1 = ldapTemplate.getContextSource().getContext(
+                "cn=marcuschiu,ou=Users,dc=example,dc=org",
+                "password"
+        );
+        DirContext dirContext2 = ldapTemplate.getContextSource().getContext(
+                "cn=admin,dc=example,dc=org",
+        "admin");
     }
 }
